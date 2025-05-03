bd3f5a3fd17a9ec5236f30be9480826b84f545ad
Karansingh B <bkaransinghb@gmail.com>
2025-05-03 18:53:08 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -9,7 +9,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', form);
+      const res = await axios.post('https://dsa-sheet-tracker-ul2c.onrender.com/api/auth/login', form);
       localStorage.setItem('token', res.data.token);
       navigate('/dashboard');
     } catch (err) {
